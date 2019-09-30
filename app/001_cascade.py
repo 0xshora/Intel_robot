ecd1de5a851c1787b1c1804a4794b29bcfca05b7
SYORA17 <112358132134.fibon@gmail.com>
2019-09-30 14:02:50 +0900

    first commit

---
+++ app/001_cascade.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+import cv2
+
+def draw_detections(img, rects, thickness=1):
+    for x, y, w, h in rects:
+        pad_w, pad_h = int(0.15 * w), int(0.05 * h)
+        cv2.rectangle(img, (x + pad_w, y + pad_h),
+                      (x + w - pad_w, y + h - pad_h), (0, 255, 0), thickness)
+
+def cascade(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # カスケードファイルの読み込み
+    face_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_frontalface_default.xml')
+    # face_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_upperbody.xml')
+    # face_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_lowerbody.xml')
+
+    faces = face_cascade.detectMultiScale(gray)
+    draw_detections(img, faces)
+    print(faces)
+    cv2.imshow('img',img)
+
+def main(mirror=True, size=None):
+    cap = cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if mirror is True:
+            frame = frame[:, ::-1]
+
+        if size is not None and len(size) == 2:
+            frame = cv2.resize(frame, size)
+
+        cascade(frame)
+
+        k = cv2.waitKey(50)
+        if k == 27:  # ESCキーで終了
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
