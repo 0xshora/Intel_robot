0f0b4cb16321ca80882b4ee2422a2934506bd59e
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2019-10-29 00:09:47 +0900

    proper format and remove the bug
    There is a redundant return 0; and } at the end

--- app/steppingmotor/motor.c
+++ app/steppingmotor/motor.c
@@ -7,7 +7,6 @@
 #include <wiringPiSPI.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
 #define BUFSIZE 32
 
 int L6470_SPI_CHANNEL;
@@ -21,22 +20,20 @@ void L6470_run_turn(long speed);
 void L6470_run_turn_moving(long speed, int right, float scale);
 void L6470_softstop();
 void L6470_softhiz();
-void L6470_speed_change(long speed, int postspeed);//change the speed from "speed" to postspeed
-void getargs(int *argc, char *argv[], char *buf);
+void L6470_speed_change(long speed, int postspeed); //change the speed from "speed" to postspeed
+void getargs(int * argc, char * argv[], char * buf);
 
-int main(int argc, char **argv){
+int main(int argc, char ** argv) {
     long speed = 0;
 
     printf("***** start spi test program *****\n");
 
     // SPI channel 0 を 1MHz で開始。
     //if (wiringPiSPISetup(L6470_SPI_CHANNEL, 1000000) < 0)
-    if (wiringPiSPISetup(0, 1000000) < 0)
-    {
+    if (wiringPiSPISetup(0, 1000000) < 0) {
         printf("SPI Setup failed:\n");
     }
-    if (wiringPiSPISetup(1, 1000000) < 0)
-    {
+    if (wiringPiSPISetup(1, 1000000) < 0) {
         printf("SPI Setup failed:\n");
     }
 
@@ -60,87 +57,86 @@ int main(int argc, char **argv){
     struct sockaddr_in serv_addr;
     struct sockaddr_in clit_addr;
 
-    if((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
-      fprintf(stderr, "socket() failed\n");
-      exit(1);
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        fprintf(stderr, "socket() failed\n");
+        exit(1);
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(serv_port);
 
-    if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-      fprintf(stderr, "bind() failed\n");
-      exit(1);
-    }
-    
-    if(listen(sockfd, 1) < 0){
-      fprintf(stderr, "listen() failed\n");
-      close(sockfd);
-      exit(1);
+    if (bind(sockfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
+        fprintf(stderr, "bind() failed\n");
+        exit(1);
     }
 
-    while(1){
-      clit_len = sizeof(clit_addr);
-      if((new_sockfd = accept(sockfd, (struct sockaddr *)&clit_addr, &clit_len)) < 0){
-        fprintf(stderr, "accept() failed\n");
+    if (listen(sockfd, 1) < 0) {
+        fprintf(stderr, "listen() failed\n");
         close(sockfd);
         exit(1);
-      }
-
-      char buf[256];
-      int buf_len;
-      int ac;
-      char *av[16];
-      memset(buf, 0, 256);
-
-      if((buf_len = read(new_sockfd, buf, 256)) < 0){
-        fprintf(stderr, "read() failed\n");
-        continue;
-      }
-      getargs(*ac, av, buf);
-
-      if(strcmp(av[0], "p") == 0){
-        long sp = atol(av[1]);
-        L6470_speed_change(speed, sp);
-	speed = sp;
-        // printf("*** Speed %ld ***\n", speed);
-      }
-
-      if(strcmp(av[0], "r") == 0 || strcmp(av[0], "l") == 0){
-        if(speed != 0){
-          double scale = atof(av[1]);
-          if(strcmp(buf, "r") == 0){
-            int right_true = 1;
-            L6470_run_turn_moving(speed, right_true, scale);
-          }else{
-            int right_false = 0;
-            L6470_run_turn_moving(speed, right_false, scale);
-          }
-	}else{
-	  long sp = atol(av[1]);
-	  if(strcmp(buf, "r") == 0){
-	    speed = 10000;
-	    L6470_run_turn(sp);
-	  }else{
-	    speed = -10000;
-	    L6470_run_turn(sp);
-	  }
-	}
-      }
-
-      if(strcmp(buf, "s") == 0){
-	L6470_speed_change(speed, 0);
-        speed = 0;
-      }
-
-      if(strcmp(buf, "e") == 0){
-        L6470_speed_change(speed, 0);
-        close(new_sockfd);
-        close(sockfd);
-        exit(0);
-        }
-      }
     }
-    return 0;
+
+    while (1) {
+        clit_len = sizeof(clit_addr);
+        if ((new_sockfd = accept(sockfd, (struct sockaddr * ) & clit_addr, & clit_len)) < 0) {
+            fprintf(stderr, "accept() failed\n");
+            close(sockfd);
+            exit(1);
+        }
+
+        char buf[256];
+        int buf_len;
+        int ac;
+        char * av[16];
+        memset(buf, 0, 256);
+
+        if ((buf_len = read(new_sockfd, buf, 256)) < 0) {
+            fprintf(stderr, "read() failed\n");
+            continue;
+        }
+        getargs( * ac, av, buf);
+
+        if (strcmp(av[0], "p") == 0) {
+            long sp = atol(av[1]);
+            L6470_speed_change(speed, sp);
+            speed = sp;
+            // printf("*** Speed %ld ***\n", speed);
+        }
+
+        if (strcmp(av[0], "r") == 0 || strcmp(av[0], "l") == 0) {
+            if (speed != 0) {
+                double scale = atof(av[1]);
+                if (strcmp(buf, "r") == 0) {
+                    int right_true = 1;
+                    L6470_run_turn_moving(speed, right_true, scale);
+                } else {
+                    int right_false = 0;
+                    L6470_run_turn_moving(speed, right_false, scale);
+                }
+            } else {
+                long sp = atol(av[1]);
+                if (strcmp(buf, "r") == 0) {
+                    speed = 10000;
+                    L6470_run_turn(sp);
+                } else {
+                    speed = -10000;
+                    L6470_run_turn(sp);
+                }
+            }
+        }
+
+        if (strcmp(buf, "s") == 0) {
+            L6470_speed_change(speed, 0);
+            speed = 0;
+        }
+
+        if (strcmp(buf, "e") == 0) {
+            L6470_speed_change(speed, 0);
+            close(new_sockfd);
+            close(sockfd);
+            exit(0);
+        }
+    }
+	return 0;
 }
