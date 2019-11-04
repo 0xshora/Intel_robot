4d1e8d18ba4e3d68a7474c0ef45b17569b351170
ysuprism <yasuhiro0317@keio.jp>
2019-11-04 11:08:14 +0900

    move host and port to send_msg()'s argumetns

--- app/client.py
+++ app/client.py
@@ -1,8 +1,6 @@
 import socket
 
-def send_msg(msg):
+def send_msg(msg, host='127.0.0.1', port='50001'):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        host = '127.0.0.1'
-        port = 50001
         s.connect((host, port))
         s.sendall(msg.encode(encoding='ascii'))
