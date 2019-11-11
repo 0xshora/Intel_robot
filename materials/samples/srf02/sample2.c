a28a4746e04ab2d33fe7e53b313037e5e3cbdf29
StyLishPoor <isstykkgd@gmail.com>
2019-11-11 10:52:48 +0900

    修正サンプルの追加

--- materials/samples/srf02/sample2.c
+++ materials/samples/srf02/sample2.c
@@ -16,7 +16,7 @@
 
 int main( void ) {
 	
-	int fd1, fd2;
+	int fd;
 	char filename[ 20 ];
 	char buf[ 10 ];
 	int res;
@@ -24,117 +24,52 @@ int main( void ) {
 
 	// I2Cデータバスをオープン
 	sprintf( filename, "/dev/i2c-1" );
-	// 1つめ
-	fd1 = open( filename, O_RDWR );
-	if ( fd1 < 0 ) {
-		printf( "Error on open\n" );
-		exit( 1 );
-	}
-	// 2つめ
-	fd2 = open( filename, O_RDWR );
-	if ( fd1 < 0 ) {
-		printf( "Error on open\n" );
+
+	fd = open( filename, O_RDWR );
+	if ( fd < 0 ) {
+		printf( "Failed to open\n" );
 		exit( 1 );
 	}
 
-	if ( ioctl( fd1, I2C_SLAVE, ( 0xE0 >> 1 ) ) < 0 ) {  // 0xE0(0x11100000) >> 1 = 0x70(0x01110000)
-		printf( "Error on slave address 0xE0\n" );
-		exit( 1 );
-	}
-	if ( ioctl( fd2, I2C_SLAVE, ( 0xE2 >> 1 ) ) < 0 ) {  // 0xE0(0x11100000) >> 1 = 0x70(0x01110000)
-		printf( "Error on slave address 0xE2\n" );
+	if ( ioctl( fd, I2C_SLAVE, ( 0xE0 >> 1 ) ) < 0 ) {  // 0xE0(0x11100000) >> 1 = 0x70(0x01110000)
+		printf( "Failed get slave address\n" );
 		exit( 1 );
 	}
 
-
-	// 1つめのセンサに対して
 	{ // read from 0xE0
 		// コマンドレジスタ0に 0x51:Real Ranging Mode - Result in centimeters を送ることによって測距が始まる
 		buf[ 0 ] = 0x00;
 		buf[ 1 ] = 0x51;
 
-		if ( ( write( fd1, buf, 2 ) ) != 2 ) {
-			printf( "0xE0 Error send the read command\n" );
+		if ( ( write( fd, buf, 2 ) ) != 2 ) {
+			printf( "Failed to set a ranging mode\n" );
 			exit( 1 );
 		}
 		// Wait for the measurement
 		// 音波だから、"行って" "帰ってくる" のに時間がかかる
 		usleep( 66000 );
 
-		// コマンドレジスタ2に対し測距値データの上位バイトをリクエスト
-		buf[ 0 ] = 0x02;
-		if ( ( write( fd1, buf, 1 ) ) != 1 ) {
-			printf( "0xE0 Error on select the Range High Byte\n" );
+		// 測距開始
+		buf[ 0 ] = 0x00;
+		if ( ( write( fd, buf, 1 ) ) != 1 ) {
+			printf( "cannot write to srf02\n" );
 			exit( 1 );
 		}
 
-		if ( ( read( fd1, buf, 1 ) ) != 1 ) {
-			printf( "0xE0 Error on read the Range High Byte\n" );
+		if ( ( read( fd, buf, 4 ) ) != 4 ) {
+			printf( "cannot read from srf02\n" );
 			exit( 1 );
 		}
-		range = buf[ 0 ] << 8;
-
-		// コマンドレジスタ3に対し測距値データの下位バイトをリクエスト
-		buf[ 0 ] = 0x03;
-		if ( ( write( fd1, buf, 1 ) ) != 1 ) {
-			printf( "0xE0 Error on select the Range Low Byte\n" );
-			exit( 1 );
-		}
-
-		if ( ( read( fd1, buf, 1 ) ) != 1 ) {
-			printf( "0xE0 Error on read the Range Low Byte\n" );
-			exit( 1 );
-		}
+		range = buf[ 2 ] << 8;
 
 		// 上位と下位をくっつける
-		range |= buf[ 0 ];
+		range |= buf[ 3 ];
 
 		printf( "0xE0 Range=%d cm\n", range );
 	}
 
-
-	// 2つめのセンサに対して
-	{ // read from 0xE2
-		buf[ 0 ] = 0x00;
-		buf[ 1 ] = 0x51;
-
-		if ( ( write( fd2, buf, 2 ) ) != 2 ) {
-			printf( "0xE2 Error send the read command\n" );
-			exit( 1 );
-		}
-		// Wait for the measurement
-		usleep( 66000 );
-
-		buf[ 0 ] = 0x02;
-		if ( ( write( fd2, buf, 1 ) ) != 1 ) {
-			printf( "0xE2 Error on select the Range High Byte\n" );
-			exit( 1 );
-		}
-
-		if ( ( read( fd2, buf, 1 ) ) != 1 ) {
-			printf( "0xE2 Error on read the Range High Byte\n" );
-			exit( 1 );
-		}
-		range = buf[ 0 ] << 8;
-
-		buf[ 0 ] = 0x03;
-		if ( ( write( fd2, buf, 1 ) ) != 1 ) {
-			printf( "0xE2 Error on select the Range Low Byte\n" );
-			exit( 1 );
-		}
-
-		if ( ( read( fd2, buf, 1 ) ) != 1 ) {
-			printf( "0xE2 Error on read the Range Low Byte\n" );
-			exit( 1 );
-		}
-		range |= buf[ 0 ];
-
-		printf("0xE2 Range=%d cm\n",range);	
-	}
-
 	// 閉じる！！
-	close( fd1 );
-	close( fd2 );
+	close( fd );
 
 	return 0;	
 }
